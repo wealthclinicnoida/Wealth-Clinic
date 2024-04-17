aedab05e10942297872b3d6ca478c66201961c5b
himanshuadsrole <himanshu.sharma@adsrole.in>
2024-04-17 22:10:25 +0530

    add

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+// module.exports = () => ({});
+module.exports = ({ env }) => ({
+    "vercel-deploy": {
+      enabled: true,
+      config: {
+        deployHook:
+          "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+        apiToken: "<vercel-api-token>",
+        appFilter: "your-app-name-on-vercel",
+        teamFilter: "your-team-id-on-vercel",
+        roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+      },
+    },
+  });
