6acf7a84db8a06d2c04713e7099b04ef7400df05
AasthaAdsrole <aastha@adsrole.net>
2024-07-16 02:55:48 +0530

    commented last fixed /logic for totalRating not working properly

--- src/api/project/controllers/project.js
+++ src/api/project/controllers/project.js
@@ -5,53 +5,51 @@
  */
 
 const { createCoreController } = require("@strapi/strapi").factories;
+module.exports = createCoreController("api::project.project");
 
-module.exports = createCoreController("api::project.project", ({ strapi }) => ({
-  async find(ctx) {
-    const { data, meta } = await super.find(ctx);
-    // console.log("==========hello=========");
+// module.exports = createCoreController("api::project.project", ({ strapi }) => ({
+//   async find(ctx) {
+//     const { data, meta } = await super.find(ctx);
+//     // console.log("==========hello=========");
 
-    const modifiedData = await Promise.all(
-      data.map(async (property) => {
-        const ratings = property.attributes.project_ratings.data;
+//     const modifiedData = await Promise.all(
+//       data.map(async (property) => {
+//         const ratings = property.attributes.project_ratings.data;
 
-        if (ratings.length > 0) {
-          const totalRating = ratings.reduce(
-            (sum, review) => sum + review.attributes.ratingStar,
-            0
-          );
-          const averageRating = totalRating / ratings.length;
-          property.attributes.totalRating = averageRating;
-        } else {
-          property.attributes.totalRating = 0;
-        }
+//         if (ratings.length > 0) {
+//           const totalRating = ratings.reduce(
+//             (sum, review) => sum + review.attributes.ratingStar,
+//             0
+//           );
+//           const averageRating = totalRating / ratings.length;
+//           property.attributes.totalRating = averageRating;
+//         } else {
+//           property.attributes.totalRating = 0;
+//         }
 
-        return property;
-      })
-    );
+//         return property;
+//       })
+//     );
 
-    return { data: modifiedData, meta };
-  },
+//     return { data: modifiedData, meta };
+//   },
 
-  async findOne(ctx) {
-    const { data } = await super.findOne(ctx);
+//   async findOne(ctx) {
+//     const { data } = await super.findOne(ctx);
 
-    const ratings = data.attributes.project_ratings.data;
+//     const ratings = data.attributes.project_ratings.data;
 
-    if (ratings.length > 0) {
-      const totalRating = ratings.reduce(
-        (sum, review) => sum + review.attributes.ratingStar,
-        0
-      );
-      const averageRating = totalRating / ratings.length;
-      data.attributes.totalRating = averageRating;
-    } else {
-      data.attributes.totalRating = 0;
-    }
+//     if (ratings.length > 0) {
+//       const totalRating = ratings.reduce(
+//         (sum, review) => sum + review.attributes.ratingStar,
+//         0
+//       );
+//       const averageRating = totalRating / ratings.length;
+//       data.attributes.totalRating = averageRating;
+//     } else {
+//       data.attributes.totalRating = 0;
+//     }
 
-    return { data };
-  },
-}));
-
-// const { createCoreController } = require("@strapi/strapi").factories;
-// module.exports = createCoreController("api::project.project");
+//     return { data };
+//   },
+// }));
