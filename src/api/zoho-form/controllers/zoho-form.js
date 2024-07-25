dd8f0b7a78ee12978b26c27b1ab8fd6127338dad
GARG-ANUJ <anujgarg2018@gmail.com>
2024-07-25 21:07:47 +0530

    code refactoring in zoho-form controller

--- src/api/zoho-form/controllers/zoho-form.js
+++ src/api/zoho-form/controllers/zoho-form.js
@@ -6,55 +6,59 @@ module.exports = {
       "https://www.zohoapis.com/crm/v2/functions/property_wala/actions/execute";
     const API_KEY =
       "1003.1e4337563bdfd4d5d09450bda8cfcf36.6c69bb448b86aa64654ef89ca2018684";
-    const OWNER_ID = 3664837000038157001; //
+    const OWNER_ID = 3664837000038157001;
+
+    const HEADER = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      params: {
+        auth_type: "apikey",
+        zapikey: API_KEY,
+      },
+    };
     try {
-      const {
-        Contact_ID,
-        Last_Name,
-        Email,
-        Mobile,
-        Query,
-        Locationn,
-        Budget,
-        Lead_Source1,
-        Lead_Type,
-        Rating,
-        Lead_Priority,
-        Stage,
-        Temp_Field_3,
-        Owner_testing,
-      } = ctx.request.body;
-
-      const response = await axios({
-        method: "post",
-        url: "https://www.zohoapis.com/crm/v2/functions/property_wala/actions/execute",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        params: {
-          auth_type: "apikey",
-          zapikey: API_KEY, // Ensure your actual API key is used
-          Contact_ID,
-          Last_Name,
-          Email,
-          Mobile,
-          Query,
-          Locationn,
-          Budget,
-          Lead_Source1,
-          Lead_Type,
-          Rating,
-          Lead_Priority,
-          Stage,
-          Temp_Field_3,
-          Owner_testing: OWNER_ID,
-        },
-      });
-
+      const zohoFormParams = getZohoParams(ctx);
+      const response = await axios.post(ZOHO_API_URL, zohoFormParams, HEADER);
       ctx.send(response.data);
     } catch (error) {
-      console.error("Error submitting form:", error);
+      console.error("Error submitting zoho form:", error);
       ctx.throw(500, "An error occurred while submitting the form");
     }
   },
+};
+
+const getZohoParams = (ctx) => {
+  const {
+    Contact_ID,
+    Last_Name,
+    Email,
+    Mobile,
+    Query,
+    Location,
+    Budget,
+    Lead_Source1,
+    Lead_Type, //whatsapp/ PhoneCall Type
+    Rating,
+    Lead_Priority,
+    Stage,
+    Temp_Field_3, // form Name
+    // Owner_testing,
+  } = ctx.request.body;
+  return {
+    Contact_ID,
+    Last_Name,
+    Email,
+    Mobile,
+    Query,
+    Location,
+    Budget,
+    Lead_Source1,
+    Lead_Type, //whatsapp/ PhoneCall Type
+    Rating,
+    Lead_Priority,
+    Stage,
+    Temp_Field_3, // form Name
+    // Owner_testing,
+  };
 };
