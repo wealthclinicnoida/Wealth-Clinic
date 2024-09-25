6e84853658af24a3376e66811153f84317b41acf
GARG-ANUJ <anujgarg2018@gmail.com>
2024-09-25 19:51:24 +0530

    applied dynamic api

--- src/api/zoho-form/controllers/zoho-form.js
+++ src/api/zoho-form/controllers/zoho-form.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 module.exports = {
   async postZohoForm(ctx) {
     const ZOHO_API_URL =
-      "https://www.zohoapis.com/crm/v2/functions/property_wala/actions/execute";
+      "https://www.zohoapis.com/crm/v2/functions/property_wala/actions/execute?";
     const API_KEY =
       "1003.1e4337563bdfd4d5d09450bda8cfcf36.6c69bb448b86aa64654ef89ca2018684";
     const OWNER_ID = 3664837000038157001;
@@ -18,10 +18,13 @@ module.exports = {
       },
     };
     try {
-      const zohoFormParams = getZohoParams(ctx);
+      // const zohoFormParams = getZohoParams(ctx);
+      // const response = await axios.post(ZOHO_API_URL, zohoFormParams, HEADER);
 
-      const response = await axios.post(ZOHO_API_URL, zohoFormParams, HEADER);
-      // console.log("==========", response.data, "------>", zohoFormParams);
+      const zohoFormParamsUrl = getZohoParams(ctx);
+      const response = await axios.post(zohoFormParamsUrl);
+
+      // console.log("==========", response.data);
       ctx.send(response.data);
     } catch (error) {
       console.error("Error submitting zoho form:", error);
@@ -48,7 +51,12 @@ const getZohoParams = (ctx) => {
     Temp_Field_3, // form Name
     // Owner_testing,
   } = ctx.request.body;
-  return {
+
+  const newUrl = `
+    https://www.zohoapis.com/crm/v2/functions/property_wala/actions/execute?auth_type=apikey&zapikey=1003.1e4337563bdfd4d5d09450bda8cfcf36.6c69bb448b86aa64654ef89ca2018684&Contact_ID=${Contact_ID}&Last_Name=${Last_Name}&Mobile=${Mobile}&Query=${Query}&Locationn=${Locationn}&Budget=${Budget}&Lead_Source1=${Lead_Source1}&Lead_Type=${Lead_Type}&Rating=${Rating}&Lead_Priority=${Lead_Priority}&Stage=${Stage}&Temp_Field_3=${Temp_Field_3}&Owner testing=${OWNER_ID}
+  `;
+
+  const newObj = {
     Contact_ID,
     Last_Name,
     Email,
@@ -64,4 +72,6 @@ const getZohoParams = (ctx) => {
     Temp_Field_3, // form Name
     Owner_testing: OWNER_ID,
   };
+
+  return newUrl;
 };
