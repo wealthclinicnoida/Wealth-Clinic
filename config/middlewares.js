1040f349699e1314b222a5b730b812b68b5492bc
Wealth Clinic <169176347+wealthclinicnoida@users.noreply.github.com>
2025-10-28 17:15:00 +0530

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,37 +2,18 @@ module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      origin: [
-        "https://wealth-clinic.com",
-        "http://localhost:3000",
-        "http://localhost:1337/",
-      ],
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      expose: ["WWW-Authenticate", "Server-Authorization"],
-      credentials: true,
-    },
-  },
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
   "strapi::session",
   "strapi::favicon",
-  {
-    name: "strapi::public",
+  "strapi::public",
+   {
+    name: 'strapi::public',
     config: {
-      maxAge: 31536000, // ✅ Cache for 1 year
-      immutable: true,  // ✅ Browser won’t revalidate unless file URL changes
-    },
-  },
-  {
-    name: "global::customCors", // Keep your custom middleware if needed
-    config: {
-      // Optional custom config
+      maxAge: 31536000, // 1 year
+      immutable: true,
     },
   },
 ];
