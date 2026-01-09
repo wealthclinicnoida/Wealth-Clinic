5070378c18b1d0176fedd256f6cf195f84360639
Wealth Clinic <169176347+wealthclinicnoida@users.noreply.github.com>
2026-01-09 13:20:16 +0530

    Add files via upload

---
+++ src/api/test-banner/controllers/test-banner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * test-banners controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::test-banner.test-banner');
---
+++ src/api/test-banner/routes/test-banner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * test-banners router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::test-banner.test-banner');
