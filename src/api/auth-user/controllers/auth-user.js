e6f22b1873ad2e032ab53653cd7923736cc42ed9
Wealth Clinic <169176347+wealthclinicnoida@users.noreply.github.com>
2026-01-23 12:35:49 +0530

    Delete src/api/auth-user directory

--- src/api/auth-user/controllers/auth-user.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * auth-user controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::auth-user.auth-user');
