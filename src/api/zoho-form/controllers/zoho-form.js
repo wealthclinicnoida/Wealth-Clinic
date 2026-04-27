19cd3be3e4d5225cc677eb87c31940c823b36c73
Wealth Clinic <169176347+wealthclinicnoida@users.noreply.github.com>
2026-04-27 15:28:03 +0530

    Update zoho-form.js

--- src/api/zoho-form/controllers/zoho-form.js
+++ src/api/zoho-form/controllers/zoho-form.js
@@ -41,7 +41,7 @@ const getZohoParams = async (ctx) => {
 
   let newUrl = "";
   if(Property_Type === "Voice AI Enquiry"){
-  newUrl = `https://www.zohoapis.com/crm/v2/functions/property_wala/actions/execute?auth_type=apikey&zapikey=1003.1e4337563bdfd4d5d09450bda8cfcf36.6c69bb448b86aa64654ef89ca2018684&Contact_ID=${Contact_ID}&Last_Name=${Last_Name}&Email=${Email}&Mobile=${Mobile}&Query=${Query}&Locationn=${Locationn}&Budget=${Budget}&Lead_Source1=${Lead_Source1}&Lead_Type=${Lead_Type}&Rating=${Rating}&Lead_Priority=${Lead_Priority}&Stage=${Stage}&Temp_Field_3=${Temp_Field_3}&Property_Type=${Property_Type}&Owner=3664837000038157001`;
+  newUrl = `https://www.zohoapis.com/crm/v2/functions/property_wala/actions/execute?auth_type=apikey&zapikey=1003.1e4337563bdfd4d5d09450bda8cfcf36.6c69bb448b86aa64654ef89ca2018684&Contact_ID=${Contact_ID}&Description=${Description}&Last_Name=${Last_Name}&Email=${Email}&Mobile=${Mobile}&Query=${Query}&Locationn=${Locationn}&Budget=${Budget}&Lead_Source1=${Lead_Source1}&Lead_Type=${Lead_Type}&Rating=${Rating}&Lead_Priority=${Lead_Priority}&Stage=${Stage}&Temp_Field_3=${Temp_Field_3}&Property_Type=${Property_Type}&Owner=3664837000038157001`;
 
   }else{
   
