1e2295d6ae2d721d8c5beca278e7170b0ce495b4
Wealth Clinic <169176347+wealthclinicnoida@users.noreply.github.com>
2026-06-25 13:08:37 +0530

    Delete src/api/registration/controller directory

--- src/api/registration/controller/registration.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * registration controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::registration.registration');
