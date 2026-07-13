cb97ad818f11bb0816f4208c37b7ae3e420db7bf
Wealth Clinic <169176347+wealthclinicnoida@users.noreply.github.com>
2026-07-13 18:17:59 +0530

    Update home-page.js

--- src/api/home-page/controllers/home-page.js
+++ src/api/home-page/controllers/home-page.js
@@ -4,131 +4,175 @@ module.exports = {
   async index(ctx) {
     try {
       const [
-        cities,
-        residentialSubCategories,
-        commercialSubCategories,
-        residentialProjects,
-        newLaunches,
-        builders,
-        blogs,
-        reviews,
-        socialLinks,
-      ] = await Promise.all([
-        // Cities
-        strapi.entityService.findMany('api::create-city.create-city', {
-          populate: '*',
-        }),
+  cities,
+  residentialSubCategories,
+  commercialSubCategories,
+  residentialProjects,
+  newLaunches,
+  builders,
+  blogs,
+  reviews,
+  socialLinks,
+] = await Promise.all([
 
-        // Residential Sub Categories
-        strapi.entityService.findMany('api::sub-category.sub-category', {
-          filters: {
-            category: {
-              Title: {
-                $eqi: 'Residential',
-              },
-            },
-          },
-          populate: '*',
-        }),
+  // Cities
+  strapi.entityService.findMany('api::create-city.create-city', {
+    fields: ['Title', 'Slug'],
+  }),
 
-        // Commercial Sub Categories
-        strapi.entityService.findMany('api::sub-category.sub-category', {
-          filters: {
-            category: {
-              Title: {
-                $eqi: 'Comercial', // Change to Commercial if that's your actual value
-              },
-            },
-          },
-          populate: '*',
-        }),
-
-        // Residential Projects
-        strapi.entityService.findMany('api::project.project', {
-          filters: {
-            category: {
-              Title: {
-                $eqi: 'Residential',
-              },
-            },
-          },
-          populate: {
-            create_city: true,
-            sub_category: true,
-            category: true,
-            Image: true,
-          },
-          limit: 1000,
-        }),
-
-        // New Launches
-        strapi.entityService.findMany('api::project.project', {
-          filters: {
-            newLaunches: true,
-          },
-          populate: {
-            Image: true,
-            sub_category: true,
-            create_city: true,
-            category: true,
-          },
-          limit: 10,
-        }),
-
-        // Priority Builders
-        strapi.entityService.findMany('api::builder.builder', {
-          filters: {
-            Priority: true,
-          },
-          populate: '*',
-        }),
-
-        // Latest Blogs
-        strapi.entityService.findMany('api::blog.blog', {
-          sort: {
-            Pubish_Date: 'desc',
-          },
-          populate: '*',
-          limit: 4,
-        }),
-
-        // Reviews
-        strapi.entityService.findMany('api::review.review', {
-          populate: '*',
-        }),
-
-        // Social Media Links
-        strapi.entityService.findMany(
-          'api::all-social-media-link.all-social-media-link',
-          {
-            populate: '*',
-          }
-        ),
-      ]);
-
-      ctx.body = {
-        success: true,
-
-        cities,
-
-        subCategories: {
-          residential: residentialSubCategories,
-          commercial: commercialSubCategories,
+  // Residential Sub Categories
+  strapi.entityService.findMany('api::sub-category.sub-category', {
+    filters: {
+      category: {
+        Title: {
+          $eqi: 'Residential',
         },
+      },
+    },
+    fields: ['Title', 'Slug'],
+  }),
 
-        projects: {
-          residential: residentialProjects,
-          newLaunches,
+  // Commercial Sub Categories
+  strapi.entityService.findMany('api::sub-category.sub-category', {
+    filters: {
+      category: {
+        Title: {
+          $eqi: 'Comercial',
         },
+      },
+    },
+    fields: ['Title', 'Slug'],
+  }),
 
-        builders,
+  // Residential Projects
+  strapi.entityService.findMany('api::project.project', {
+    filters: {
+      category: {
+        Title: {
+          $eqi: 'Residential',
+        },
+      },
+    },
+    fields: [
+      'Project_Name',
+      'Slug',
+      'Min_Price',
+      'Max_Price',
+      'Project_Status',
+      'Possession_Status',
+      'RERA',
+      'Project_Tag',
+      'Featured',
+      'newLaunches'
+    ],
+    populate: {
+      Image: {
+        fields: ['url', 'alternativeText']
+      },
+      create_city: {
+        fields: ['Title', 'Slug']
+      },
+      sub_category: {
+        fields: ['Title', 'Slug']
+      },
+      category: {
+        fields: ['Title']
+      }
+    },
+    limit: 1000,
+  }),
 
-        blogs,
+  // New Launches
+  strapi.entityService.findMany('api::project.project', {
+    filters: {
+      newLaunches: true,
+    },
+    fields: [
+      'Project_Name',
+      'Slug',
+      'Min_Price',
+      'Max_Price',
+      'Project_Status'
+    ],
+    populate: {
+      Image: {
+        fields: ['url']
+      },
+      create_city: {
+        fields: ['Title', 'Slug']
+      },
+      sub_category: {
+        fields: ['Title']
+      },
+    },
+    limit: 10,
+  }),
 
-        reviews,
+  // Builders
+  strapi.entityService.findMany('api::builder.builder', {
+    filters: {
+      Priority: true,
+    },
+    fields: [
+      'Builder_Name',
+      'Slug'
+    ],
+    populate: {
+      Logo: {
+        fields: ['url']
+      }
+    },
+  }),
 
-        socialLinks,
-      };
+  // Blogs
+  strapi.entityService.findMany('api::blog.blog', {
+    sort: {
+      Pubish_Date: 'desc',
+    },
+    fields: [
+      'Title',
+      'Slug',
+      'Short_Description',
+      'Pubish_Date',
+      'Read_Time'
+    ],
+    populate: {
+      Featured_Image: {
+        fields: ['url']
+      }
+    },
+    limit: 4,
+  }),
+
+  // Reviews
+  strapi.entityService.findMany('api::review.review', {
+    fields: [
+      'Name',
+      'Designation',
+      'Rating',
+      'Review'
+    ],
+    populate: {
+      Image: {
+        fields: ['url']
+      }
+    },
+  }),
+
+  // Social Links
+  strapi.entityService.findMany(
+    'api::all-social-media-link.all-social-media-link',
+    {
+      fields: [
+        'Facebook',
+        'Instagram',
+        'LinkedIn',
+        'Twitter',
+        'Youtube'
+      ],
+    }
+  ),
+]);
     } catch (error) {
       console.error(error);
       ctx.throw(500, error);
