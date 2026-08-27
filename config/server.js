f4e93d08571bf866fd0bdc53a52539ef547c95ba
Wealth Clinic <169176347+wealthclinicnoida@users.noreply.github.com>
2026-08-27 11:22:57 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,11 @@
+"use strict";
+
+const cronTasks = require("./cron-tasks");
+
 module.exports = ({ env }) => ({
-  // console.log("APP_KEYS:", env.array("APP_KEYS"))
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+
   app: {
     keys: env.array("APP_KEYS"),
   },
@@ -10,15 +14,22 @@ module.exports = ({ env }) => ({
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 
+  // Enable Strapi cron jobs
+  cron: {
+    enabled: true,
+    tasks: cronTasks,
+  },
+
   // Gmail Email Configuration
   email: {
     gmail: {
       user: env("GMAIL_USER"),
       appPassword: env("GMAIL_APP_PASSWORD"),
     },
+
     recipientList: [
-      // Add recipient emails here or use RECIPIENT_EMAILS env variable
-      // { email: 'recipient@example.com', name: 'Recipient Name' }
+      // Add recipient emails here
+      // { email: "recipient@example.com", name: "Recipient Name" }
     ],
   },
 });
