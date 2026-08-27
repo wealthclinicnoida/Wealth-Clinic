f582183d08fc2507c7baec4213580a069b781f30
Wealth Clinic <169176347+wealthclinicnoida@users.noreply.github.com>
2026-08-27 11:24:29 +0530

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,7 +5,6 @@ module.exports = [
   {
     name: "strapi::cors",
     config: {
-      enabled: true,
       origin: ["*"], // ✅ Allow all origins
       headers: ["Content-Type", "Authorization", "Origin", "Accept"],
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
